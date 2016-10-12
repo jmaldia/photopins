90f0d35aa0b0271c9ac1a164cb088c7ad0c9b9b5
Jon Maldia <jon@maldia.net>
2016-10-12 15:27:47 -0400

    Committing changes

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Make password stronger
+  validates :password, :presence => true,
+                      :confirmation => true,
+                      :length => {:within => 6..40},
+                      :on => :create,
+                      :if => :password,
+                      :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
+end
