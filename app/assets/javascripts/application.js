068810f6e09ad9c7dc39c775c4c3c962fc413d85
Jon Maldia <jon@maldia.net>
2016-10-12 16:09:36 -0400

    Fixed issue with sign out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require_tree .
+//
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
-//= require_tree .
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#home'
+  root "pages#home"
   get "about" => "pages#about"
   get "my_pins" => "pages#my_pins"
 
   # get 'pages/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    get 'users/sign_out' => "devise/sessions#destroy"
+  end
 end
