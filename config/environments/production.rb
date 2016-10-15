0ddc7fbdbbd11e9d9e88cca36e0b38ecaf6963b1
Jon Maldia <jon@maldia.net>
2016-10-15 13:57:41 -0400

    Missing a comma on env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,7 @@ Rails.application.configure do
     :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],  # S3_BUCKET_NAME - old name
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
     :region => ENV['AWS_DEFAULT_REGION']
     }
   }
