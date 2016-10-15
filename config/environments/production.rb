b4e4bd97807af5dc01eb386a1d62732bd27a7f8c
Jon Maldia <jon@maldia.net>
2016-10-15 13:56:19 -0400

    Added AWS region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,6 +100,7 @@ Rails.application.configure do
     :bucket => ENV['AWS_BUCKET'],  # S3_BUCKET_NAME - old name
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :region => ENV['AWS_DEFAULT_REGION']
     }
   }
 end
