ce09c51e6cbfb284220fd75355706aba739e26eb
Jon Maldia <jon@maldia.net>
2016-10-16 13:30:16 -0400

    Changed look, added user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :name, presence: true
+
   # Make password stronger
-  validates :password, :presence => true,
-                      :confirmation => true,
-                      :length => {:within => 6..40},
-                      :on => :create,
-                      :if => :password,
-                      :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
+  # validates :password, :presence => true,
+  #                     :confirmation => true,
+  #                     :length => {:within => 6..40},
+  #                     :on => :create,
+  #                     :if => :password,
+  #                     :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
 
   has_many :pins
 end
